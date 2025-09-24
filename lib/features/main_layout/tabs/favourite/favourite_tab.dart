29fe0ddb7cc23ace3a5308cfc8f9f34f76d79211
Karim-Elbahnasawy <gamalkarim928@gmail.com>
2025-09-24 20:03:12 +0300

    finish_favourite_ui

--- lib/features/main_layout/tabs/favourite/favourite_tab.dart
+++ lib/features/main_layout/tabs/favourite/favourite_tab.dart
@@ -1,10 +1,55 @@
+import 'package:evently_app/core/resourses/colors_manager.dart';
+import 'package:evently_app/features/main_layout/tabs/home_tab/event_item.dart';
+import 'package:evently_app/models/category_model.dart';
+import 'package:evently_app/models/event_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FavouriteTab extends StatelessWidget {
   const FavouriteTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: REdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                prefixIconColor: ColorsManager.blue,
+                hintText: 'Search for events',
+                hintStyle: Theme.of(context).textTheme.displaySmall,
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(color: ColorsManager.blue, width: 1),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(color: ColorsManager.blue, width: 1),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 16.h),
+          Expanded(
+            child: ListView.separated(
+              itemCount: 10,
+              itemBuilder: (context, index) => EventItem(
+            event: EventModel(
+              category: CategoryModel.categoriesWithAll[3],
+              title: 'This is a Birthday Party ',
+              description: 'This is a Birthday Party ',
+              dateTime: DateTime.now(),
+              timeOfDay: TimeOfDay.now(),
+            ),
+          ),
+              separatorBuilder: (context, index) => SizedBox(height: 16.h),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
